efa9d68fca15751f2e16b8a79fa1f98606699fd7
Dmitriy Antonov <79038945169@yandex.ru>
2022-02-07 15:55:43 +0300

    Color and count of operation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-
+    static long count = 0;
     public static void main(String[] args) {
         double start, finish;
         start = System.currentTimeMillis();
-        for (int i=0; i<=50;i++){
-           // System.out.println("Фибоначчи(" + i + ")=" + nFib(i));
+        for (int i=0; i<=25;i++){
+            System.out.println("Фибоначчи(" + i + ")=" + nFib(i));
+            System.out.println("количество вызовов функции: " + count);
         }
         //рисуем фракталы
         new DrawFractals();
@@ -19,6 +20,7 @@ public class Main {
     }
     //числа Фибоначчи
     public static long nFib (int n){
+        count++;
         if (n == 0 || n == 1)
             return 1;
         return nFib(n-1) + nFib(n - 2);
@@ -27,22 +29,28 @@ public class Main {
 
 class DrawFractals extends JFrame{
     Paint paint;
-    int W = 600, H = 600;
+    int W = 800, H = 800;
     //рисование
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.setColor(Color.BLUE);
-        int r = 150;
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(0,0,W,H);
+        int r = 200;
+        g.setColor(Color.GREEN);
         drawCircles(g,W/2-r,H/2-r,2*r);
     }
     //рекурсия для рисования
     static void drawCircles(Graphics g, int x, int y, int r){
          g.drawOval(x, y, r, r);
          if (r>20){
+             g.setColor(Color.RED);
              drawCircles(g,x-r/4,y+r/4,r/2);
+             g.setColor(Color.YELLOW);
              drawCircles(g,x+r/4,y-r/4,r/2);
+             g.setColor(Color.CYAN);
              drawCircles(g,x+r/4*3 ,y+r/4,r/2);
+             g.setColor(Color.ORANGE);
              drawCircles(g,x+r/4,y+r/4*3,r/2);
          }
     }
